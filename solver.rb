9c502098e1ed5e53923911b1a072e8d8e84fa7a5
SamanAtashi <milady00126@gmail.com>
2021-12-24 12:31:01 +0300

    Add fizzbuz method

--- solver.rb
+++ solver.rb
@@ -1,16 +1,23 @@
 class Solver
   def factorial(n)
-     return 1 if n<=1
-    n*factorial(n-1)
+    return 1 if n <= 1
+
+    n * factorial(n - 1)
   end
 
   def reverse(str)
-   str.reverse!
+    str.reverse!
   end
 
-  def fizzbuzz; end
+  def fizzbuzz(sth)
+    if (sth % 15).zero?
+      return 'fizzbuzz'
+    elsif (sth % 5).zero?
+      return 'buzz'
+    elsif (sth % 3).zero?
+      return 'fizz'
+    end
+
+    'not divisable'
+  end
 end
-
-temp = Solver.new
-
-puts temp.reverse("sam")
